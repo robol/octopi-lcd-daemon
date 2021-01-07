537ebdde035559442e0005a83186c48d1c96d5ad
Leonardo Robol <leo@robol.it>
2021-01-07 14:50:37 +0000

    Moved API key into configuration file.

--- lcd-daemon.py
+++ lcd-daemon.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
-API_KEY = "C2B8D8369DCC4DF1B307CC78AD134B59"
-
-import requests, time, json, lcddriver
+import requests, time, json, lcddriver, configparser, sys, os
 
 class LcdDaemon():
 
@@ -11,10 +9,18 @@ class LcdDaemon():
         self._lcd.lcd_clear()
         self.define_symbols()
 
+        if not os.path.exists('lcd-daemon.conf'):
+            print("lcd-daemon.conf not found, aborting!")
+            sys.exit(1)
+
+        config = configparser.ConfigParser()
+        config.read('lcd-daemon.conf')
+
+        self._api_key = config['octopi']['api_key']
+        self._host = config['octopi']['host']
+
     def define_symbols(self):
-        # Select 0x00 in CGRAM
-        self._lcd.lcd_write(0x00 | lcddriver.LCD_SETCGRAMADDR)
-        # Write the custom symbol
+        # Clock symbol as 0x00
         clock = [ 0b00001110, \
                   0b00000100, \
                   0b00001110, \
@@ -22,16 +28,29 @@ class LcdDaemon():
                   0b00010111, \
                   0b00010001, \
                   0b00001110 ]
+        self.save_symbol(0x00, clock)
 
-        for row in clock:
+        deg = [ 0b11000, \
+                0b11000, \
+                0b00000, \
+                0b00111, \
+                0b01000, \
+                0b01000, \
+                0b01000, \
+                0b00111 ]
+        self.save_symbol(0x08, deg)
+
+    def save_symbol(self, address, rows):
+        self._lcd.lcd_write(address | lcddriver.LCD_SETCGRAMADDR)
+        for row in rows:
             self._lcd.lcd_write(row, lcddriver.Rs)
 
     def update_jobs(self):
         try:
             job_r = requests.get(
-                'http://octopi.oleandri.tk/api/job',
+                'http://%s/api/job' % self._host,
                 headers = {
-                    'X-Api-Key': API_KEY
+                    'X-Api-Key': self._api_key
                 }
             )
             jobs = json.loads(job_r.text)
@@ -42,8 +61,8 @@ class LcdDaemon():
     def update_printer(self):
         try:
             printer_r = requests.get(
-                'http://octopi.oleandri.tk/api/printer',
-                headers = { 'X-Api-Key': API_KEY }
+                'http://%s/api/printer' % self._host,
+                headers = { 'X-Api-Key': self._api_key }
             )
             printer = json.loads(printer_r.text)
             self._printer = printer
@@ -107,9 +126,9 @@ class LcdDaemon():
             tool_target = temps["tool0"]["target"]
             bed_target = temps["bed"]["target"]
             if counter == 0:
-                self.set_message(2, "Tool: %3.0fC%s" % (tool_temp, '' if tool_target in [ None, 0.0 ] else '/%3.0fC' % tool_target))
+                self.set_message(2, "Tool: %3.0f\x01%s" % (tool_temp, '' if tool_target in [ None, 0.0 ] else '/%3.0fC' % tool_target))
             elif counter == 1:
-                self.set_message(2, "Bed: %2.0fC%s" % (bed_temp, '' if bed_target in [ None, 0.0 ] else '/%2.0fC' % bed_target))
+                self.set_message(2, "Bed: %2.0f\x01%s" % (bed_temp, '' if bed_target in [ None, 0.0 ] else '/%2.0fC' % bed_target))
             else:
                 if progress is not None:
                     nboxes = round(progress["completion"] / 100.0 * 15.0) + 1
